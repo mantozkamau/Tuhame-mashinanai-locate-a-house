d989f848db8b9513c865364fb7f2a69d59fe1b32
george kamau <flexkamau646@gmail.com>
2020-01-30 08:22:40 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function check(form)
 {
-if(form.username.value == "antony" && form.pwd.value === "1234")
+if(form.userId.value === "antony" && form.pwd.value === "1234")
 {
     alert ("Login successfully");
     window.location = "userhomepage.html"; 
     return false;
 }
-else if(form.username.value == "kelvinkyalo" && form.pwd.value === "56789")
+else if(form.userId.value === "kelvinkyalo" && form.pwd.value === "56789")
 {
     window.location = "userhomepage.html"; 
     return false;
 }
-else if(form.username.value == "george" && form.pwd.value === "0000")
+else if(form.userId.value === "george" && form.pwd.value === "0000")
 {
     window.location = "userhomepage.html"; 
     return false;
 }
-else if(form.username.value == "brenda" && form.pwd.value === "5555")
+else if(form.userId.value === "brenda" && form.pwd.value === "5555")
 {
     window.location = "userhomepage.html"; 
     return false;
